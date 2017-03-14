6dfa2b562dd462984703cc0b68455f6d6568a42a
Martin Nilsson <martin.h.nilsson@gmail.com>
2017-03-14 22:12:34 +0100

    finds build id

--- fetch-build-artifact-task/index.ts
+++ fetch-build-artifact-task/index.ts
@@ -53,8 +53,6 @@ async function run() {
         let buildsUri = projectUri + '/_apis/build/builds';
         task.debug('buildsUri=' + buildsUri);
 
-        //let buildPath = '/' + project + '/_apis/build/builds?definitions=' + definitionId + '&statusFilter=completed&resultFilter=succeeded&$top=1&api-version=2.0';
-
         var buildsOptions = {
             uri: buildsUri,
             auth: {
@@ -73,51 +71,31 @@ async function run() {
         task.debug('buildsOption:');
         task.debug(JSON.stringify(buildsOptions));
 
+        console.log('Querying completed successful builds of build definition\'' + definitionId + '\'');
+
         await requestPromise(buildsOptions)
-            .then(function (build) {
-                console.log(build);
+            .then(function (builds: any[]) {
+                if (builds.length === 0){
+                    task.debug('no builds found for project');
+                    throw new Error('Could not find project \'' + project + '\'. Make sure \'Allow Scripts to Access OAuth Token\' is enabled and that the project exists.');
+                }
+
+                let build = builds[0];
+                task.debug('build:');
+                task.debug(JSON.stringify(build));
+
+                return build.id;
             })
-            .catch(function (err) {
-                console.log(err);
+            .then(function(buildId){
+                console.log(buildId);
+            })
+            .then(function(){
+                console.log('Done');
             });
 
-
-        /*var options = {
-            host: 'bool1sandbox.visualstudio.com',
-            path: buildPath,
-            headers: authHeader,
-            method: 'GET'
-        };
-
-        await getBuilds(options).catch((error) => {
-            console.log(error);
-        }).then((buildId) => {
-            console.log('build id:');
-            console.log(buildId);
-        });*/
     } catch (error) {
         task.setResult(task.TaskResult.Failed, error.message);
     }
-}
-
-async function getBuilds(options: object) {
-    return get(options).then((body: any) => {
-        return body.value.id;
-    })
-}
-
-async function get(options: object) {
-    return new Promise((resolve, reject) => {
-        const request = https.get(options, (response) => {
-            if (response.statusCode < 200 || response.statusCode > 299) {
-                reject(new Error('Failed to load page, status code: ' + response.statusCode));
-            }
-            const body = [];
-            response.on('data', (chunk) => body.push(chunk));
-            response.on('end', () => resolve(body.join('')));
-        });
-        request.on('error', (err) => reject(err))
-    })
 }
 
 run();
